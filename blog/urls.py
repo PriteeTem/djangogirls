40e5548e2cb717aee3822e0405e00ffaf098ff8f
PriteeT <pritee@alum.mit.edu>
2017-12-27 16:55:30 -0600

    events page update

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
-    url(r'events^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^about$', views.about_page, name='about_page'),
     url(r'^team$', views.team_page, name='team_page'),
+    url(r'^events$', views.post_list, name='post_list'),
 ]
